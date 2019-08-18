a555879415bef27721f9749881cf1a00e1fdfc4c
Jeff Dawson <jeff@fejalish.com>
2019-08-18 18:52:47 -0400

    slackbot with token config

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ ADD app /app
 
 WORKDIR /app
 
+RUN \
+  apk add --no-cache bash git openssh && \
+  go get -v github.com/shomali11/slacker
+
 RUN go build -o /usr/local/bin/main .
 
 CMD ["/usr/local/bin/main"]
--- app/main.go
+++ app/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+  "context"
+  "fmt"
+  "github.com/shomali11/slacker"
+  "log"
+  "os"
+  "strconv"
+)
 
 func main() {
-  fmt.Println("hello slack")
+  fmt.Println("slackbot starting...")
+
+  // TODO: check for slack token and exit if doesn't exist
+
+  bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), slacker.WithDebug(getEnvAsBool("DEBUG", false)))
+
+  definition := &slacker.CommandDefinition{
+    Handler: func(request slacker.Request, response slacker.ResponseWriter) {
+      response.Reply("pong")
+    },
+  }
+
+  bot.Command("ping", definition)
+
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+
+  err := bot.Listen(ctx)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func getEnvAsBool(name string, defaultVal bool) bool {
+    valStr := os.Getenv(name)
+    if val, err := strconv.ParseBool(valStr); err == nil {
+  return val
+    }
+
+    return defaultVal
 }
